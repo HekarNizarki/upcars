bed016b3f4fd89d9be0b48a6eba846f606bfaff3
Hekar A. Mohammed <hekar1987@gmail.com>
2023-04-06 09:23:48 +0300

    int

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class homeeee extends StatefulWidget {
+  const homeeee({Key? key}) : super(key: key);
+
+  @override
+  State<homeeee> createState() => _homeeeeState();
+}
+
+class _homeeeeState extends State<homeeee> {
+  @override
+   _A
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      home: Scaffold(body: Column(children: [
+
+        Text("Name"),
+        Text("Phone"),
+        Text("Email"),
+        Text("Password"),
+      TextField(
+        controller: _a,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Name',
+        ),
+      ),
+
+      )
+
+
+      ],)),
+    );
+  }
+}
